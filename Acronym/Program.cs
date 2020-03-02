8443743ff8970703e36f37517efbdfc54ec81078
Kyran <k.b_bryant@icloud.com>
2020-03-02 14:00:57 -0600

    FizzBuzz Assignment, Need to debug Fizz & Buzz Count

--- Acronym/Program.cs
+++ Acronym/Program.cs
@@ -7,24 +7,26 @@ namespace CPU_github
         static void Main(string[] args)
         {
             System.Console.WriteLine("Enter in some text please: ");
-           string nString = System.Console.ReadLine();
+            string nString = System.Console.ReadLine();
 
-           eString(nString);
+            eString(nString);
 
 
         }
 
 
+
+
         static void eString(string str)
         {
-        
 
-                string[] sString = str.Split(" ");
 
-                for (int i = 0; i < sString.Length; i++)
-                {
-                    Console.Write(sString[i].Substring(0, 1));
-                }
+            string[] sString = str.Split(" ");
+
+            for (int i = 0; i < sString.Length; i++)
+            {
+                Console.Write(sString[i].Substring(0, 1));
+            }
         }
 
 
---
+++ fizzbuzz-project/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace fizzbuzz_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int fbuzz = 1000;
+
+           for (int i = 0; i <= fbuzz; i++)
+           {
+
+               if(i % 3 == 0)
+               {
+                    System.Console.Write(i + " " + "Fizz " + " ");
+               }
+
+                 if(i % 5 == 0)
+               {
+                   System.Console.Write(i + " " + "Buzz " + " ");
+
+               }
+
+
+                 if(i % 5 == 0 && i % 3 == 0)
+               {
+                   System.Console.WriteLine("(Fizz" + "Buzz)");
+               }
+
+           }
+
+
+           int[] nFizz = {fbuzz};
+               int[] nBuzz = {fbuzz};
+
+               for (int j = 0; j < nFizz.Length; j++)
+               {
+                   if(j % 3 == 0)
+                   {
+                       System.Console.WriteLine("There are " + nFizz[j] + " Fizz");
+                   }
+               }
+
+
+               for (int k = 0; k < nBuzz.Length; k++)
+               {
+                   if(k % 5 == 0)
+                   {
+                       System.Console.WriteLine("There are " + nFizz[k] + " Buzz");
+                   }
+               }
+        }
+    }
+}
